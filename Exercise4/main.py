2e7aa6353bd89acbf96bd527387514b8bec72b75
SveinKare <sksorestad@gmail.com>
2022-11-03 22:09:37 +0100

    Update main.py

--- Exercise4/main.py
+++ Exercise4/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env pybricks-micropython
+#This robot is a slightly more advanced line-following robot, which was used to compete against other robots on a track. 
+#The robot was quite robust, and made semi finals. It was sadly too slow to win. 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -8,13 +10,6 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 import threading
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-######################################################
-#          DENNE KODEN SVINGER PÅ SVART              #
-######################################################
-
-# Create your objects here.
 ev3 = EV3Brick()
 høyreSensor = ColorSensor(Port.S1)
 venstreSensor = ColorSensor(Port.S2)
@@ -28,7 +23,7 @@ startButton = TouchSensor(Port.S3)
 red = 45
 green = 45
 blue = 45
-
+#A function for the right side sensor. Causes the robot to turn right if it picks up on the right sensor, but not the left. Turn rate increases, and caps out at 60 degrees.
 def høyre():
     turnRate = 0
     while(True):
@@ -38,16 +33,17 @@ def høyre():
             while (measuredH < 65):
                 measuredV = venstreSensor.reflection()
                 if measuredV < 65:
-                    base.drive(175, 0)
+                    base.drive(125, 0)
                     turnRate = 0
-                elif (turnRate < 60): #Før: 60
+                elif (turnRate < 60)
                     turnRate = turnRate + 1
-                    base.drive(125, turnRate) #100 fungerer
+                    base.drive(125, turnRate)
                 measuredH = høyreSensor.reflection()
             turnRate = 0
             base.drive(175, 0)
 
-
+#A function for the left side sensor. Does the same thing as the previous function, but for the left side. 
+#Can perform sharper turns than the right side due to the track going counterclockwise.
 def venstre():
     turnRate = 0
     while(True):
@@ -66,13 +62,13 @@ def venstre():
             turnRate = 0
             base.drive(175, 0)
             
-
+#The program runs the sensors in separate threads, increasing speed and making the code a bit easier to understand. 
 base.drive(175,0)
 threadH = threading.Thread(target=høyre, args=())
 threadV = threading.Thread(target=venstre, args=())
 threadH.start()
 threadV.start()
-
+#dirtyTricks is essentially a bucket of small lego pieces that was emptied onto the track to sabotage other robots. 
 while (not startButton.pressed()):
     continue
 wait(19500)
